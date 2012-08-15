04d3f4724ba6b92fde511969ff775bdb22b8b1b4
Beau Simensen <beau@dflydev.com>
2012-08-15 09:12:44 -0700

    CS fixes.

--- src/Dflydev/Twig/Extension/GitHubGist/Cache/CacheInterface.php
+++ src/Dflydev/Twig/Extension/GitHubGist/Cache/CacheInterface.php
@@ -11,30 +11,40 @@
 
 namespace Dflydev\Twig\Extension\GitHubGist\Cache;
 
+/**
+ * Cache Interface.
+ *
+ * @author Beau Simensen <beau@dflydev.com>
+ */
 interface CacheInterface
 {
     /**
      * Determine if a gist exists
+     *
      * @param unknown_type $id
      */
     public function exists($id);
 
     /**
      * Get the content of a gist
+     *
      * @param string $id
+     *
      * @return array
      */
     public function get($id);
 
     /**
      * Set the content of a gist
-     * @param string $id
-     * @param array $content
+     *
+     * @param string $id      ID
+     * @param array  $content Content
      */
     public function set($id, $content);
 
     /**
      * Expire a gist
+     *
      * @param string $id
      */
     public function expire($id);
